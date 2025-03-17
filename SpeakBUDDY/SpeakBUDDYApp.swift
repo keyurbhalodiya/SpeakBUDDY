3217d6a84d5ea6c25082bb4be57526a236c57cc2
Keyur Bhalodiya | KB <kbit920@outlook.com>
2025-03-17 23:02:18 +0900

    Initial Commit

---
+++ SpeakBUDDY/SpeakBUDDYApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakBUDDYApp.swift
+//  SpeakBUDDY
+//
+//  Created by Keyur Bhalodiya on 2025/03/17.
+//
+
+import SwiftUI
+
+@main
+struct SpeakBUDDYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
